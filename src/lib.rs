60f39187dff2a559ff21fc2f4051ba313bdd7cf4
Aleksey Kuznetsov <zummenix@gmail.com>
2015-11-08 17:40:52 +0500

    Implement an unit test for 'files' function.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@
 //! files. Main purpose of this crate is to contain functions and types
 //! for **mprovision**.
 
+#[cfg(test)]
+#[macro_use(expect)]
+extern crate expectest;
+#[cfg(test)]
+extern crate tempdir;
+
 use std::fs::{self, DirEntry};
 use std::path::{Path, PathBuf};
 use std::env;
@@ -50,4 +56,25 @@ pub fn directory() -> Result<PathBuf> {
     env::home_dir()
         .map(|path| path.join("Library/MobileDevice/Provisioning Profiles"))
         .ok_or("couldn't find home directory".into())
+}
+
+#[cfg(test)]
+mod tests {
+    use expectest::prelude::*;
+    use tempdir::TempDir;
+    use std::fs::File;
+    use super::files;
+
+    #[test]
+    fn filter_mobileprovision_files() {
+        let temp_dir = TempDir::new("test").unwrap();
+        let result = files(temp_dir.path()).map(|iter| iter.count());
+        expect!(result).to(be_ok().value(0));
+
+        File::create(temp_dir.path().join("1.mobileprovision")).unwrap();
+        File::create(temp_dir.path().join("2.mobileprovision")).unwrap();
+        File::create(temp_dir.path().join("3.txt")).unwrap();
+        let result = files(temp_dir.path()).map(|iter| iter.count());
+        expect!(result).to(be_ok().value(2));
+    }
 }
