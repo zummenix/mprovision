e74ff37d60898972e3886117892dac96e30ca93d
Aleksey Kuznetsov <zummenix@gmail.com>
2015-11-14 21:53:28 +0500

    Remove count command.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,10 @@
 extern crate mprovision;
 extern crate clap;
 
-use std::fs;
 use clap::{Arg, App, AppSettings, SubCommand};
 
 fn main() {
     let subcommands = vec![
-        SubCommand::with_name("count")
-            .about("Counts provisioning profiles in a directory.")
-            .arg(Arg::with_name("DIRECTORY")
-                .help("Directory where to count provisioning profiles.")
-                .required(false)),
         SubCommand::with_name("search")
             .about("Searches provisioning profile for provided text.")
             .arg(Arg::with_name("TEXT")
@@ -29,9 +23,6 @@ fn main() {
                       .subcommands(subcommands)
                       .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("count") {
-        handle_count_subcommand(matches)
-    }
     if let Some(matches) = matches.subcommand_matches("search") {
         let s = matches.value_of("TEXT").unwrap();
         match mprovision::search(matches.value_of("DIRECTORY"), s) {
@@ -43,25 +34,6 @@ fn main() {
                     }
                 }
             },
-            Err(err) => println!("Error: {}", err),
-        }
-    }
-}
-
-fn handle_count_subcommand(matches: &clap::ArgMatches) {
-
-    fn show(result: mprovision::Result<Box<Iterator<Item = fs::DirEntry>>>) {
-        match result {
-            Ok(files) => println!("Found {} files.", files.count()),
-            Err(err) => println!("Error: {}", err),
-        }
-    }
-
-    if let Some(directory) = matches.value_of("DIRECTORY") {
-        show(mprovision::files(directory));
-    } else {
-        match mprovision::directory() {
-            Ok(directory) => show(mprovision::files(directory)),
             Err(err) => println!("Error: {}", err),
         }
     }
