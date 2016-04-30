6a482a11d7e89475d9736d6551550e1e9e58cc71
Aleksey Kuznetsov <zummenix@gmail.com>
2016-04-30 14:40:36 +0500

    Simplify Display for Error.

--- src/error.rs
+++ src/error.rs
@@ -30,10 +30,7 @@ impl error::Error for Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Error::Io(ref e) => e.fmt(f),
-            Error::Own(ref e) => e.fmt(f),
-        }
+        error::Error::description(self).fmt(f)
     }
 }
 
