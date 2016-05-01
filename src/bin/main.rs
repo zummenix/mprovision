63d4ffc57785fc788e21ac22ccae36a44aa92073
Aleksey Kuznetsov <zummenix@gmail.com>
2016-05-01 11:27:35 +0500

    Refactor usage of <directory> parameter.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,6 +7,7 @@ extern crate chrono;
 
 use std::io::{self, Write};
 use std::process;
+use std::path::Path;
 use docopt::Docopt;
 
 const USAGE: &'static str = "
@@ -80,14 +81,8 @@ fn search(args: &::docopt::ArgvMap) -> Result<(), String> {
     if text.is_empty() {
         return Err("<text> should not be empty.".to_owned());
     }
-    let dir_name = args.get_str("<directory>");
-    let dir_name = if dir_name.is_empty() {
-        None
-    } else {
-        Some(dir_name.as_ref())
-    };
 
-    mprovision::with_path(dir_name, |path| mprovision::search(path, text))
+    mprovision::with_path(directory(args), |path| mprovision::search(path, text))
         .and_then(|info| {
             if info.profiles.len() == 0 {
                 println!("Nothing found for '{}'", text);
@@ -109,14 +104,8 @@ fn remove(args: &::docopt::ArgvMap) -> Result<(), String> {
     if uuid.is_empty() {
         return Err("<uuid> should not be empty.".to_owned());
     }
-    let dir_name = args.get_str("<directory>");
-    let dir_name = if dir_name.is_empty() {
-        None
-    } else {
-        Some(dir_name.as_ref())
-    };
 
-    mprovision::with_path(dir_name, |path| mprovision::remove(path, uuid))
+    mprovision::with_path(directory(args), |path| mprovision::remove(path, uuid))
         .and_then(|_| Ok(println!("Profile '{}' was removed.", uuid)))
         .map_err(|e| e.to_string())
 }
@@ -126,14 +115,8 @@ fn show_xml(args: &::docopt::ArgvMap) -> Result<(), String> {
     if uuid.is_empty() {
         return Err("<uuid> should not be empty.".to_owned());
     }
-    let dir_name = args.get_str("<directory>");
-    let dir_name = if dir_name.is_empty() {
-        None
-    } else {
-        Some(dir_name.as_ref())
-    };
 
-    mprovision::with_path(dir_name, |path| mprovision::xml(path, uuid))
+    mprovision::with_path(directory(args), |path| mprovision::xml(path, uuid))
         .and_then(|xml| Ok(println!("{}", xml)))
         .map_err(|e| e.to_string())
 }
@@ -141,14 +124,8 @@ fn show_xml(args: &::docopt::ArgvMap) -> Result<(), String> {
 fn show_expired(args: &::docopt::ArgvMap) -> Result<(), String> {
     use chrono::*;
 
-    let dir_name = args.get_str("<directory>");
-    let dir_name = if dir_name.is_empty() {
-        None
-    } else {
-        Some(dir_name.as_ref())
-    };
-
-    mprovision::with_path(dir_name, |path| mprovision::expired_profiles(path, UTC::now()))
+    mprovision::with_path(directory(args),
+                          |path| mprovision::expired_profiles(path, UTC::now()))
         .and_then(|info| {
             if info.profiles.len() == 0 {
                 println!("All profiles are valid.");
@@ -163,4 +140,13 @@ fn show_expired(args: &::docopt::ArgvMap) -> Result<(), String> {
             Ok(())
         })
         .map_err(|e| e.to_string())
+}
+
+fn directory(args: &::docopt::ArgvMap) -> Option<&Path> {
+    let dir_name = args.get_str("<directory>");
+    if dir_name.is_empty() {
+        None
+    } else {
+        Some(dir_name.as_ref())
+    }
 }
