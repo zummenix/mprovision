bfce763d4e342d799961e875b35c527a6fe3b6b9
Aleksey Kuznetsov <zummenix@gmail.com>
2016-06-08 14:48:16 +0500

    Introduce CliError and CliResult types. Refactor.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,6 +8,8 @@ extern crate chrono;
 use std::io::{self, Write};
 use std::process;
 use std::path::Path;
+use std::error;
+use std::fmt;
 use docopt::Docopt;
 use mprovision as mp;
 
@@ -52,7 +54,7 @@ impl<'a> Command<'a> {
         Command { args: args }
     }
 
-    fn execute(&self) -> Result<(), String> {
+    fn execute(&self) -> CliResult {
         if self.args.get_bool("search") {
             search(self.args)
         } else if self.args.get_bool("remove") {
@@ -64,111 +66,145 @@ impl<'a> Command<'a> {
         } else if self.args.get_bool("remove-expired") {
             remove_expired(self.args)
         } else {
-            Err("Command is not implemented".to_string())
+            Err(CliError::Custom("Command is not implemented".to_string()))
         }
     }
 }
 
-fn search(args: &::docopt::ArgvMap) -> Result<(), String> {
+type CliResult = std::result::Result<(), CliError>;
+
+#[derive(Debug)]
+enum CliError {
+    Lib(mp::Error),
+    Custom(String),
+    EmptyParameter(&'static str),
+}
+
+impl error::Error for CliError {
+    fn description(&self) -> &str {
+        match *self {
+            CliError::Lib(ref e) => e.description(),
+            CliError::Custom(ref e) => e,
+            CliError::EmptyParameter(_) => "parameter is empty",
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        if let CliError::Lib(ref e) = *self {
+            Some(e)
+        } else {
+            None
+        }
+    }
+}
+
+impl fmt::Display for CliError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            CliError::Lib(ref e) => error::Error::description(e).fmt(f),
+            CliError::Custom(ref e) => e.fmt(f),
+            CliError::EmptyParameter(p) => write!(f, "{} should not be empty.", p),
+        }
+    }
+}
+
+impl From<mp::Error> for CliError {
+    fn from(e: mp::Error) -> Self {
+        CliError::Lib(e)
+    }
+}
+
+fn search(args: &::docopt::ArgvMap) -> CliResult {
     let text = args.get_str("<text>");
     if text.is_empty() {
-        return Err("<text> should not be empty.".to_owned());
+        return Err(CliError::EmptyParameter("<text>"));
     }
 
-    mp::with_dir(directory(args), |dir| mp::search(dir, text))
-        .and_then(|info| {
-            if info.profiles.is_empty() {
-                println!("Nothing found for '{}'", text);
-            } else {
-                for profile in &info.profiles {
-                    println!("\n{}", profile.description());
-                }
-                println!("\nFound {} of {}", info.profiles.len(), info.total);
-            }
-            Ok(())
-        })
-        .map_err(|e| e.to_string())
+    let info = try!(mp::with_dir(directory(args), |dir| mp::search(dir, text)));
+    if info.profiles.is_empty() {
+        println!("Nothing found for '{}'", text);
+    } else {
+        for profile in &info.profiles {
+            println!("\n{}", profile.description());
+        }
+        println!("\nFound {} of {}", info.profiles.len(), info.total);
+    }
+    Ok(())
 }
 
-fn remove(args: &::docopt::ArgvMap) -> Result<(), String> {
+fn remove(args: &::docopt::ArgvMap) -> CliResult {
     let uuid = args.get_str("<uuid>");
     if uuid.is_empty() {
-        return Err("<uuid> should not be empty.".to_owned());
+        return Err(CliError::EmptyParameter("<uuid>"));
     }
 
-    mp::with_dir(directory(args), |dir| mp::remove(dir, uuid))
-        .and_then(|_| Ok(println!("'{}' was removed", uuid)))
-        .map_err(|e| e.to_string())
+    let _ = try!(mp::with_dir(directory(args), |dir| mp::remove(dir, uuid)));
+    println!("'{}' was removed", uuid);
+    Ok(())
 }
 
-fn show_xml(args: &::docopt::ArgvMap) -> Result<(), String> {
+fn show_xml(args: &::docopt::ArgvMap) -> CliResult {
     let uuid = args.get_str("<uuid>");
     if uuid.is_empty() {
-        return Err("<uuid> should not be empty.".to_owned());
+        return Err(CliError::EmptyParameter("<uuid>"));
     }
 
-    mp::with_dir(directory(args), |dir| mp::xml(dir, uuid))
-        .and_then(|xml| Ok(println!("{}", xml)))
-        .map_err(|e| e.to_string())
+    let xml = try!(mp::with_dir(directory(args), |dir| mp::xml(dir, uuid)));
+    println!("{}", xml);
+    Ok(())
 }
 
-fn show_expired(args: &::docopt::ArgvMap) -> Result<(), String> {
+fn show_expired(args: &::docopt::ArgvMap) -> CliResult {
     use chrono::*;
 
     let mut date = UTC::now();
     if let Some(days) = args.get_str("<days>").parse::<i64>().ok() {
         if days < 0 || days > 365 {
-            return Err("<days> should be between 0 and 365".to_owned());
+            return Err(CliError::Custom("<days> should be between 0 and 365".to_string()));
         }
         date = date + Duration::days(days);
     }
 
-    mp::with_dir(directory(args), |dir| mp::expired_profiles(dir, date))
-        .and_then(|info| {
-            if info.profiles.is_empty() {
-                println!("All provisioning profiles are valid");
-            } else {
-                for profile in &info.profiles {
-                    println!("\n{}", profile.description());
-                }
-                println!("\nFound {} of {}", info.profiles.len(), info.total);
-            }
-            Ok(())
-        })
-        .map_err(|e| e.to_string())
+    let info = try!(mp::with_dir(directory(args), |dir| mp::expired_profiles(dir, date)));
+    if info.profiles.is_empty() {
+        println!("All provisioning profiles are valid");
+    } else {
+        for profile in &info.profiles {
+            println!("\n{}", profile.description());
+        }
+        println!("\nFound {} of {}", info.profiles.len(), info.total);
+    }
+    Ok(())
 }
 
-fn remove_expired(args: &::docopt::ArgvMap) -> Result<(), String> {
+fn remove_expired(args: &::docopt::ArgvMap) -> CliResult {
     use chrono::*;
 
-    mp::with_dir(directory(args), |dir| mp::expired_profiles(dir, UTC::now()))
-        .and_then(|info| {
-            if info.profiles.is_empty() {
-                println!("All provisioning profiles are valid");
-            } else {
-                let mut errors_counter = 0;
-                let mut removals_counter = 0;
-                for profile in info.profiles {
-                    match std::fs::remove_file(&profile.path) {
-                        Ok(_) => {
-                            removals_counter += 1;
-                            println!("'{}' was removed", profile.uuid)
-                        }
-                        Err(e) => {
-                            errors_counter += 1;
-                            println!("Error while trying to remove '{}'", profile.uuid);
-                            println!("{}", e);
-                        }
-                    }
+    let info = try!(mp::with_dir(directory(args), |dir| mp::expired_profiles(dir, UTC::now())));
+    if info.profiles.is_empty() {
+        println!("All provisioning profiles are valid");
+    } else {
+        let mut errors_counter = 0;
+        let mut removals_counter = 0;
+        for profile in info.profiles {
+            match std::fs::remove_file(&profile.path) {
+                Ok(_) => {
+                    removals_counter += 1;
+                    println!("'{}' was removed", profile.uuid)
                 }
-                println!("Removed {} of {}", removals_counter, info.total);
-                if errors_counter > 0 {
-                    println!("Errors: {}", errors_counter);
+                Err(e) => {
+                    errors_counter += 1;
+                    println!("Error while trying to remove '{}'", profile.uuid);
+                    println!("{}", e);
                 }
             }
-            Ok(())
-        })
-        .map_err(|e| e.to_string())
+        }
+        println!("Removed {} of {}", removals_counter, info.total);
+        if errors_counter > 0 {
+            println!("Errors: {}", errors_counter);
+        }
+    }
+    Ok(())
 }
 
 fn directory(args: &::docopt::ArgvMap) -> Option<&Path> {
