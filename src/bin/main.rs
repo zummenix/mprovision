b7303d12c39c38d316f8d66d8bb25db048ce67c6
Aleksey Kuznetsov <zummenix@gmail.com>
2017-01-26 21:22:44 +0500

    Reformat

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -17,16 +17,14 @@ use chrono::*;
 mod cli;
 
 fn main() {
-    let result = cli::parse(env::args()).and_then(|command| {
-        match command {
-            Command::List(cli::ListParams { filter, expire_in_days, directory }) => {
-                list(filter, expire_in_days, directory)
-            }
-            Command::ShowUuid(uuid, directory) => show_uuid(uuid, directory),
-            Command::ShowFile(path) => show_file(path),
-            Command::Remove(uuid, directory) => remove(uuid, directory),
-            Command::Clean(directory) => clean(directory),
+    let result = cli::parse(env::args()).and_then(|command| match command {
+        Command::List(cli::ListParams { filter, expire_in_days, directory }) => {
+            list(filter, expire_in_days, directory)
         }
+        Command::ShowUuid(uuid, directory) => show_uuid(uuid, directory),
+        Command::ShowFile(path) => show_file(path),
+        Command::Remove(uuid, directory) => remove(uuid, directory),
+        Command::Clean(directory) => clean(directory),
     });
     if let Err(e) = result {
         e.exit();
@@ -44,28 +42,24 @@ fn list(filter: Option<String>,
             let total = entries.len();
             let date = expires_in_days.map(|days| UTC::now() + Duration::days(days));
             let filter_string = filter.as_ref();
-            let mut profiles = mp::filter(entries, |profile| {
-                match (date, filter_string) {
-                    (Some(date), Some(string)) => {
-                        profile.expiration_date <= date && profile.contains(string)
-                    }
-                    (Some(date), _) => profile.expiration_date <= date,
-                    (_, Some(string)) => profile.contains(string),
-                    (_, _) => true,
+            let mut profiles = mp::filter(entries, |profile| match (date, filter_string) {
+                (Some(date), Some(string)) => {
+                    profile.expiration_date <= date && profile.contains(string)
                 }
+                (Some(date), _) => profile.expiration_date <= date,
+                (_, Some(string)) => profile.contains(string),
+                (_, _) => true,
             });
             profiles.sort_by(|a, b| a.creation_date.cmp(&b.creation_date));
             (total, profiles)
         })
-        .and_then(|(total, profiles)| {
-            if profiles.is_empty() {
-                Ok(println!("Nothing found"))
-            } else {
-                for profile in &profiles {
-                    println!("\n{}", profile.description());
-                }
-                Ok(println!("\nFound {} of {}", profiles.len(), total))
+        .and_then(|(total, profiles)| if profiles.is_empty() {
+            Ok(println!("Nothing found"))
+        } else {
+            for profile in &profiles {
+                println!("\n{}", profile.description());
             }
+            Ok(println!("\nFound {} of {}", profiles.len(), total))
         })
 }
 
@@ -108,26 +102,22 @@ fn clean(directory: Option<PathBuf>) -> Result<(), cli::Error> {
             let date = UTC::now();
             mp::filter(entries, |profile| profile.expiration_date <= date)
         })
-        .and_then(|profiles| {
-            if profiles.is_empty() {
-                Ok(println!("All provisioning profiles are valid"))
-            } else {
-                profiles.iter()
-                    .map(|profile| {
-                        std::fs::remove_file(&profile.path)
-                            .map(|_| format!("'{}' was removed\n", profile.uuid))
-                            .map_err(|err| format!("'{}' {}\n", profile.uuid, err))
-                    })
-                    .fold(Ok(String::new()), |acc, s| {
-                        match (acc, s) {
-                            (Ok(acc), Ok(s)) => Ok(concat(acc, s)),
-                            (Ok(acc), Err(s)) |
-                            (Err(acc), Ok(s)) |
-                            (Err(acc), Err(s)) => Err(concat(acc, s)),
-                        }
-                    })
-                    .map(|s| println!("{}", s))
-                    .map_err(cli::Error::Custom)
-            }
+        .and_then(|profiles| if profiles.is_empty() {
+            Ok(println!("All provisioning profiles are valid"))
+        } else {
+            profiles.iter()
+                .map(|profile| {
+                    std::fs::remove_file(&profile.path)
+                        .map(|_| format!("'{}' was removed\n", profile.uuid))
+                        .map_err(|err| format!("'{}' {}\n", profile.uuid, err))
+                })
+                .fold(Ok(String::new()), |acc, s| match (acc, s) {
+                    (Ok(acc), Ok(s)) => Ok(concat(acc, s)),
+                    (Ok(acc), Err(s)) |
+                    (Err(acc), Ok(s)) |
+                    (Err(acc), Err(s)) => Err(concat(acc, s)),
+                })
+                .map(|s| println!("{}", s))
+                .map_err(cli::Error::Custom)
         })
 }
